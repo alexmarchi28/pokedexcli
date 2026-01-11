e72258d925acff606604816377a9baf0b0b0975a
alexmarchi28 <alex.marchi2000@gmail.com>
2026-01-11 14:24:46 +0100

    initial REPL structure

--- repl.go
+++ repl.go
@@ -1,9 +1,32 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 	toLower := strings.ToLower(text)
 	words := strings.Fields(toLower)
 	return words
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		rawInput := scanner.Text()
+		cleanedInput := cleanInput(rawInput)
+		if len(cleanedInput) == 0 {
+			continue
+		}
+
+		firstWord := cleanedInput[0]
+
+		fmt.Printf("Your command was: %s\n", firstWord)
+	}
 }
